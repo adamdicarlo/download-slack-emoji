c7029bca9552df24ed3bde48f0033e060e730b11
Adam DiCarlo <97462+adamdicarlo@users.noreply.github.com>
2020-08-21 10:21:27 -0700

    :sparkles: Update to use API with admin token

--- index.js
+++ index.js
@@ -3,56 +3,65 @@
 'use strict'
 
 const fs = require('fs')
+const path = require('path')
 const chalk = require('chalk')
-const cheerio = require('cheerio')
 const got = require('got')
-const limit = require('promise-limit')(10)
+const fetch = require('node-fetch')
+const limit = require('promise-limit')(50)
 
-const $ = cheerio.load(fs.readFileSync('./emoji-page.html'))
-const emojis = []
-
-$('.emoji_row > td:first-child > span').each((index, el) => {
-  const url = $(el).data('original')
-  const segments = url.split('/')
-  const ext = segments[segments.length - 1].split('.')[1]
-  const name = segments[segments.length - 2] + `.${ext}`
-  emojis.push({ name, url })
-})
-
-const stats = {
-  skips: 0,
-  errors: 0,
-  successes: 0
+if (!process.env.SLACK_TOKEN) {
+  console.log('Missing environment variable SLACK_TOKEN!')
+  process.exit(1)
 }
 
-Promise.all(emojis.map(emoji => {
-  return limit(() => downloadOne(emoji))
-})).then(() => {
+async function run() {
+  const response = await fetch(`https://slack.com/api/emoji.adminList?token=${process.env.SLACK_TOKEN}&count=10000`)
+  if (response.status !== 200) {
+    console.error(response)
+    return
+  }
+
+  const { emoji } = await response.json()
+
+  const stats = {
+    skips: 0,
+    errors: 0,
+    successes: 0
+  }
+
+  await Promise.all(
+    emoji
+      .filter(emoji => !emoji.is_alias)
+      .map(emoji => limit(() => downloadOne(emoji)))
+  )
+
   console.log(chalk.bold.cyan('Woohoo, we’re done! 🎉'))
   console.log(chalk.green(`• ${stats.successes} emoji downloaded to ./emoji/`))
   console.log(chalk.red(`• ${stats.errors} emoji failed to download`))
   console.log(chalk.cyan(`• ${stats.skips} emoji skipped (files already existed)`))
-})
 
-function downloadOne (emoji) {
-  return new Promise((resolve, reject) => {
-    const filename = `./emoji/${emoji.name}`
-    if (fs.existsSync(filename)) {
-      stats.skips++
-      resolve()
-      return
-    }
+  function downloadOne (emoji) {
+    return new Promise((resolve) => {
+      const ext = path.extname(emoji.url)
+      const filename = `./emoji/${emoji.name}${ext}`
+      if (fs.existsSync(filename)) {
+        stats.skips++
+        resolve()
+        return
+      }
 
-    got.stream(emoji.url).pipe(fs.createWriteStream(filename))
-      .on('error', (error) => {
-        console.error(chalk.bold.red(`Error retrieving ${emoji.name}`), error)
-        stats.errors++
-        resolve()
-      })
-      .on('close', () => {
-        console.log(chalk.bold.green(`Retrieved ${emoji.name}`))
-        stats.successes++
-        resolve()
-      })
-  })
+      got.stream(emoji.url).pipe(fs.createWriteStream(filename))
+        .on('error', (error) => {
+          console.error(chalk.bold.red(`Error retrieving ${emoji.name}`), error)
+          stats.errors++
+          resolve()
+        })
+        .on('close', () => {
+          stats.successes++
+          resolve()
+        })
+    })
+  }
 }
+
+run()
